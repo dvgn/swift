db64075690b83dc2e7692f9448b902bf4a90e2ee
geun <drgeun@naver.com>
2017-03-13 01:16:43 +0900

    New Project

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblHello: UILabel!
+    
+    @IBOutlet weak var txtName: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "hello, " + txtName.text!
+    }
 
 }
 
